fd28a2a331e032e34b01aac20a7bbc9044f647d4
Riyad <Riyad9876@gmail.com>
2023-08-08 14:46:24 -0400

    render todo items

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,8 +1,20 @@
-import 
-
+import { useSelector, useDispatch } from "react-redux"
+import { addTodo } from "../features/todos/todosSlice"
 
 export default function ToDoList() {
+    const todos = useSelector((state) => state.todos) //getting state from redux store
+    console.log(todos);
+
+    const dispatch = useDispatch()
+
+
+
     return(
+        <>
         <h1>ToDoList</h1>
+        <ul>{todos.map(todo => <li key={todo.text}>{todo.text}</li>)}</ul>
+        <input type="text"/>
+        <button>Add Todo</button>
+        </>
     )
 }
--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -5,16 +5,18 @@ import { createSlice } from "@reduxjs/toolkit";
 export const todosSlice = createSlice({
     name: 'todos',
     initialState: [
-        {text: 'Learn React', completed: true},
-        {text: 'Learn Redux', completed: false}
+        {id: '1', text: 'Learn React', completed: true},
+        {id: '2', text: 'Learn Redux', completed: false}
     ],
     reducers: {
-        //add todo
+        addTodo: (state) => {
+
+        }
 
     }
 })
 
 
 // Todo: need to export actions creators
-
+export const { addTodo } = todosSlice.actions
 export default todosSlice.reducer;
